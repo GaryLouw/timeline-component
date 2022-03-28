8a92e09ce8a2bc2fcde8f2f2d9e4a0f890ded156
Gary <developer.garylouw@gmail.com>
2022-03-28 12:08:27 +0200

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Timeline from './components/Timeline';
+import './App.css';
+
+const App = () => (
+  <div>
+  <h1>React Timeline Component</h1>
+  <Timeline />
+</div>
+);
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,64 @@
+const data = [
+    {
+        text: 'Medium dummy text',
+        date: 'March 7 2020',
+        category:{
+            tag:'medium',
+            color: '#018f69'
+        },
+        link:{
+            url: '',
+            text:'Read more'
+        }
+    },
+    {
+        text: 'App ideas dummy text',
+        date: 'December 3 2021',
+        category:{
+            tag:'app-ideas',
+            color: '#fff'
+        },
+        link:{
+            url: '',
+            text:'Check it out'
+        }
+    },
+    {
+        text: 'Improve coding dummy text',
+        date: 'January 20 2022',
+        category:{
+            tag:'blog',
+            color: '#e17b77'
+        },
+        link:{
+            url: '',
+            text:'Check it out here'
+        }
+    },
+    {
+        text: 'Additional text',
+        date: 'January 20 2022',
+        category:{
+            tag:'blog',
+            color: '#e17b77'
+        },
+        link:{
+            url: '',
+            text:'Check it out here'
+        }
+    },
+    {
+        text: 'Additional coding dummy text',
+        date: 'February 17 2021',
+        category:{
+            tag:'blog',
+            color: '#e3e3e3'
+        },
+        link:{
+            url: '',
+            text:'Check it out here'
+        }
+    }
+];
+
+export default data;
